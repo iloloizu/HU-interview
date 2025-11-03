a84f456cff6e8781e1ebf5f0c7d5b64783bfad12
Ilolo Izu <ilolo@Ilolos-MacBook-Pro.local>
2025-11-03 01:36:40 -0600

    Initialize React + TypeScript + Vite project with essential configurations and components. Added ESLint and Tailwind CSS setup, along with a basic file structure including pages, components, and state management. Included mock data for courses and user profiles, and established routing for the application.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const inCI = process.env.GITHUB_ACTIONS === 'true'
+  const repo = process.env.GITHUB_REPOSITORY?.split('/')?.[1]
+  const base = inCI && repo ? `/${repo}/` : '/'
+  return {
+    base,
+    plugins: [react()],
+  }
 })
