ff5fc07210b944f30ddad0c88b6921287a779e52
Ilolo Izu <ilolo@Ilolos-MacBook-Pro.local>
2025-11-03 02:03:35 -0600

    Update BrowserRouter to use basename from environment variables for improved routing configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { AppStateProvider } from './state/AppState'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppStateProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </AppStateProvider>
